be7417c73dba566836ff237df5ee00cb17f594a7
MatthewG-Tech <matthewbguerra@yahoo.com>
2019-03-30 17:19:44 -0700

    Nav Bar

--- JavaScript/project.js
+++ JavaScript/project.js
@@ -1,19 +1,27 @@
-var navigationBar;
+var navigationElement, footerElement;
 
 function start(){
     initalize();
-    navigationBarSetup();
+    navigationElementSetup();
+    footerSetup();
 }
 //Initallize Variables
 function initalize(){
-    navigationBar=document.getElementById("navigationBar");
+    navigationElement=document.getElementById("navigationBar");
+    footerElement=document.getElementById("footer");
 }
 //Create Navigation Bar
-//Inspired by W3 Schools: https://www.w3schools.com/howto/howto_css_fixed_sidebar.asp
-function navigationBarSetup(){
-    var output = "<a href=\"#\">Testing</a>"
-    output += "<a href=\"#\">Testing</a>"
-    navigationBar.innerHTML = output;
+//Inspired by W3 Schools: https://www.w3schools.com/css/tryit.asp?filename=trycss_navbar_horizontal_black_right
+function navigationElementSetup(){
+    var output = "<nav>\n<ul>\n<li><a href=\"index.html\">Matthew Guerra</a></li>\n";
+    output += "<li><a href=\"developer.html\">Developer</a></li>\n";
+    output += "<li><a href=\"photography.html\">Photography</a></li>\n";
+    output += "<li><a href=\"showProduction.html\">Show Production</a></li>\n";
+    output += "<li><a href=\"resources.html\">Resources</a></li>\n</ul>\n</nav>";
+    navigationElement.innerHTML = output;
 }
-
+function footerSetup(){
+    var output = "<footer>\nMatthew Guerra &copy; 2019\n</footer>";
+    footerElement.innerHTML = output;
+}
 window.addEventListener("load",start,false);
